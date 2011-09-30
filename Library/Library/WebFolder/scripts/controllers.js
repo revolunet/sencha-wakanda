f8768758956169097c8823e3889d4f32b7ba4923
goldledoigt <gary@revolunet.com>
2011-09-30 17:24:05 +0200

    various fix + details

--- Library/Library/WebFolder/scripts/controllers.js
+++ Library/Library/WebFolder/scripts/controllers.js
@@ -5,13 +5,35 @@ Ext.regController('books', {
             record = interaction.record,
             store = record.books(),
             list = new Ext.List({
+                flex: 1,
                 store: store,
-                itemTpl: '{title}'
-            });
-        
-        list = panel.add(list);
-        panel.setActiveItem(list);
+                itemTpl: '{title}',
+                listeners: {
+                    selectionchange: function(list, records) {
+                        if (!records.length) return;
+                        var record = records[0];
+                        info.update(record.data);
+                    }
+                }
+            }),
+            info = new Ext.Panel({
+                flex: 1,
+                scroll: 'vertical',
+                styleHtmlContent: true,
+                tpl: '<div>{title}</div>'
+                    + '<div>{isbn}</div>'
+                    + '<div>{editionDate}</div>'
+                    + '<div>{description}</div>'
+            }),
+            card = {
+                items: [list, info],
+                layout: {type: 'vbox', align: 'stretch'}
+            };
+
+        card = panel.add(card);
+        panel.setActiveItem(card);
         panel.dockedItems.first().items.first().show();
+        list.select(0);
     }
 
 });
