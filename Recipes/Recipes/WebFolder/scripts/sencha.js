2f523f9e54857d731c0483bf6f5b9b1a20842ec0
goldledoigt <gary@revolunet.com>
2011-10-11 04:31:31 +0200

    commit from spain

--- Recipes/Recipes/WebFolder/scripts/sencha.js
+++ Recipes/Recipes/WebFolder/scripts/sencha.js
@@ -170,16 +170,109 @@ Ext.onReady(function() {
 
     liveStore.guaranteeRange(0, 199);
 
+
+    /**** ANIMATED CHARTS *****************************************************/
+
+    Ext.regModel('cpuStats', {
+        fields: [
+            {name: 'pFree', mapping: 'vm_stat.pages_free'},
+            {name: 'pActive', mapping: 'vm_stat.pages_active'},
+            {name: 'pInactive', mapping: 'vm_stat.pages_inactive'},
+            {name: 'pSpeculative', mapping: 'vm_stat.pages_speculative'},
+            {name: 'pWiredDown', mapping: 'vm_stat.pages_wired_down'},
+            {name: 'translationFaults', mapping: 'vm_stat.translation_faults'},
+            {name: 'pCopyOnWrite', mapping: 'vm_stat.pages_copy_on_write'},
+            {name: 'pZeroFilled', mapping: 'vm_stat.pages_zero_filled'},
+            {name: 'pReactivated', mapping: 'vm_stat.pages_reactivated'},
+            {name: 'pIns', mapping: 'vm_stat.pageins'},
+            {name: 'pOuts', mapping: 'vm_stat.pageouts'},
+            {name: 'ioCache', mapping: 'vm_stat.object_cache'},
+            {name: 'ioUser', mapping: 'iostat.user'},
+            {name: 'ioSystem', mapping: 'iostat.system'},
+            {name: 'ioIdle', mapping: 'iostat.idle'},
+            {name: 'ioOne', mapping: 'iostat.onem'},
+            {name: 'ioFive', mapping: 'iostat.fivem'},
+            {name: 'ioFifteen', mapping: 'iostat.fifteenm'}
+        ],
+        proxy: {
+            type: 'rest',
+            url: '/rest/Computer/cpuStats',
+            reader: {
+                type: 'json',
+                root: 'result'
+            }
+        }
+    });
+
+    window.chartStore = Ext.create('Ext.data.Store', {
+        model: 'cpuStats',
+        listeners: {
+            load: function(store, records) {
+                var data = [], r;
+                for (var i = 0, l = records.length; i < l; i++) {
+                    r = records[i];
+                    // data.push({load: r.get('ioCache'), label: 'cache'});
+                    data.push({load: r.get('ioIdle'), label: 'idle'});
+                    data.push({load: r.get('ioUser'), label: 'user'});
+                    data.push({load: r.get('ioSystem'), label: 'system'});
+                }
+                pieStore.loadData(data);
+                // pieChart.redraw();
+            }
+        }
+    });
+
+    window.pieStore = Ext.create('Ext.data.Store', {
+        fields: [
+            {name: 'load', type: 'number'},
+            {name: 'label', type: 'string'}
+        ]
+    });
+
+    window.pieChart = Ext.create('Ext.chart.Chart', {
+        animate: true,
+        store: pieStore,
+        shadow: true,
+        legend: {
+            position: 'right'
+        },
+        insetPadding: 60,
+        series: [{
+            type: 'pie',
+            field: 'load',
+            showInLegend: true,
+            highlight: {
+                segment: {
+                    margin: 20
+                }
+            },
+            label: {
+                field: 'label',
+                display: 'rotate',
+                contrast: true,
+                font: '18px Arial'
+            }
+        }]
+    });
+
     Ext.create('Ext.Panel', {
         x: 540,
         y: 540,
         width: 500,
         height: 500,
         frame: true,
-        autoScroll: true,
+        layout: 'fit',
         floating: true,
+        items: pieChart,
         title: 'Wakanda Charts',
         renderTo: document.body
+    });
+
+    Ext.TaskManager.start({
+        interval: 1000,
+        run: function() {
+            chartStore.load();
+        }
     });
 
 });
