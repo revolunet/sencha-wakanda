277533e543d37f1df8b0c121772d0f1c58be31dd
goldledoigt <gary@revolunet.com>
2011-10-15 20:11:34 +0200

    enable chart in sencha touch exmaple

--- Library/Library/WebFolder/scripts/models.js
+++ Library/Library/WebFolder/scripts/models.js
@@ -1,23 +1,21 @@
-Ext.regModel('Book', {
-    fields: [
-        {name: 'isbn', type: 'string'},
-        {name: 'title', type: 'string'},
-        {name: 'editionDate', type: 'date'},
-        {name: 'description', type: 'string'},
-        {name: 'author', type: 'string', mapping: 'author.__deferred.__KEY'}
-    ],
-    proxy: {type: 'wakanda'}
-});
+    Ext.regModel('Book', {
+        fields: [
+            {name: 'isbn', type: 'string'},
+            {name: 'title', type: 'string'},
+            {name: 'editionDate', type: 'date'},
+            {name: 'description', type: 'string'},
+            {name: 'author', type: 'string', mapping: 'author.__deferred.__KEY'}
+        ],
+        proxy: {type: 'wakanda'}
+    });
 
-Ext.regModel('Author', {
-    idProperty: '__KEY',
-    // extend: 'wakanda',
-    // fields: []
-    fields: [
-        {name: '__KEY', type: 'string'},
-        {name: 'name', type: 'string'},
-        {name: 'booksCount', type: 'number'}
-    ],
-    proxy: {type: 'wakanda'},
-    hasMany: {model: 'Book', name: 'books', expand: true, foreignKey: 'author'}
-});
+    Ext.regModel('Author', {
+        idProperty: '__KEY',
+        fields: [
+            {name: '__KEY', type: 'string'},
+            {name: 'name', type: 'string'},
+            {name: 'booksCount', type: 'number'}
+        ],
+        proxy: {type: 'wakanda'},
+        hasMany: {model: 'Book', name: 'books', expand: true, foreignKey: 'author'}
+    });
